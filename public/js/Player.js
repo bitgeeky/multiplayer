2220d40605180b762cea1048d3fcc58b62ae4c27
root <root@localhost.localdomain>
2013-10-27 01:21:49 +0530

    initial commit

---
+++ public/js/Player.js
@@ -0,0 +1,73 @@
+/**************************************************
+** GAME PLAYER CLASS
+**************************************************/
+var Player = function(startX, startY) {
+	var heroready=false;
+	var heroimage=new Image();
+	heroimage.onload=function(){
+		heroready=true;
+	};
+	heroimage.src="images/hero.png";
+	var x = startX,
+		y = startY,
+		id,
+		moveAmount = 7;
+	
+	// Getters and setters
+	var getX = function() {
+		return x;
+	};
+
+	var getY = function() {
+		return y;
+	};
+
+	var setX = function(newX) {
+		x = newX;
+	};
+
+	var setY = function(newY) {
+		y = newY;
+	};
+
+	// Update player position
+	var update = function(keys) {
+		// Previous position
+		var prevX = x,
+			prevY = y;
+
+		// Up key takes priority over down
+		if (keys.up) {
+			y -= moveAmount;
+		} else if (keys.down) {
+			y += moveAmount;
+		};
+
+		// Left key takes priority over right
+		if (keys.left) {
+			x -= moveAmount;
+		} else if (keys.right) {
+			x += moveAmount;
+		};
+
+		return (prevX != x || prevY != y) ? true : false;
+	};
+
+	// Draw player
+	var draw = function(ctx) {
+		if(heroready){
+	//	ctx.fillRect(x-5, y-5, 10, 10);
+		ctx.drawImage(heroimage,x-5,y-5);
+		}
+	};
+
+	// Define which variables and methods can be accessed
+	return {
+		getX: getX,
+		getY: getY,
+		setX: setX,
+		setY: setY,
+		update: update,
+		draw: draw
+	}
+};
